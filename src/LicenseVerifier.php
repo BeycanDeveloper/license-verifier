d63268e851fcf10aa1515a2599cc32560d12e637
Halil Beycan <50718965+BeycanDeveloper@users.noreply.github.com>
2022-03-29 17:39:00 +0300

    added new param

--- src/LicenseVerifier.php
+++ src/LicenseVerifier.php
@@ -10,9 +10,10 @@ class LicenseVerifier
      * It checks the validity of the purchase licenseCode you entered and returns true false.
      * 
      * @param string $licenseCode
+     * @param string|null $productcode
      * @return bool
      */
-    public static function verify(string $licenseCode) : ?object
+    public static function verify(string $licenseCode, ?string $productcode = null) : ?object
     {
 
         $headers = ["Content-Type: application/json"];
@@ -24,7 +25,8 @@ class LicenseVerifier
             CURLOPT_REFERER => $_SERVER["SERVER_NAME"],
             CURLOPT_HTTPHEADER => $headers,
             CURLOPT_POSTFIELDS => json_encode([
-                "licenseCode" => trim($licenseCode)
+                "licenseCode" => trim($licenseCode),
+                "productCode" => trim($productcode)
             ]),
             CURLOPT_SSL_VERIFYHOST => false,
             CURLOPT_SSL_VERIFYPEER => false
@@ -35,5 +37,14 @@ class LicenseVerifier
         curl_close($curl);
         
         return $resp ? $resp : null;
+    }
+
+    /**
+     * @param string $url
+     * @return void
+     */
+    public static function setVerifyURL(string $url)
+    {
+        self::$verifyURL = $url;
     }
 }
